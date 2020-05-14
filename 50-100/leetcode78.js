318c00663ae6f343ef80627c3c6ae349a1ac8ec4
SkynI25 <rlxo1651@daum.net>
2020-05-14 12:05:34 +0900

    Leetcode78 solution

---
+++ 50-100/leetcode78.js
@@ -0,0 +1,54 @@
+let nums = [0];
+
+(function () {
+    // backtracking
+    let answer = [];
+    let k = 0;
+    let n = 0;
+    debugger
+    function recursive(idx=0, arr=[]) {
+        if(arr.length === k) {
+            answer.push(arr);
+        }
+        for(let i = idx; i < n; i++) {
+            arr.push(nums[i]);
+            recursive(i+1, arr.concat());
+            arr.pop();
+        }
+    }
+    n = nums.length;
+    for(k = 0; k < n+1; k++) {
+        recursive();
+    }
+    return answer;
+
+    // first solution
+    // let answer = [];
+    // function recursive(arr, idx) {
+    //     if(arr.length === nums.length - 1) {
+    //         return;
+    //     }
+    //     let recursiveArr = [];
+    //     for(let i = idx + 1; i < nums.length; i++) {
+    //         recursiveArr = arr.concat();
+    //         recursiveArr.push(nums[i]);
+    //         answer.push(recursiveArr);
+    //         recursive(recursiveArr, i);
+    //     }
+    // }
+    // debugger
+    // if(nums.length > 1) {
+    //     answer.push([]);
+    //     answer.push(nums);
+    //     for(let i = 0; i < nums.length; i++) {
+    //         let arr = [];
+    //         arr.push(nums[i]);
+    //         answer.push(arr);
+    //         recursive(arr, i);
+    //     }
+    // } else if(nums.length === 1){
+    //     answer.push([]);
+    //     answer.push(nums);
+    // }
+    // return answer;
+})();
