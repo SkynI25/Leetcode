7723a3d1675af55e3b0bbbad455ff2da17ad85aa
SkynI25 <rlxo1651@daum.net>
2020-05-22 10:59:53 +0900

    Leetcode91 solution

--- 50-100/leetcode91.js
+++ 50-100/leetcode91.js
@@ -1,5 +1,24 @@
-let s = "";
+let s = "1222222";
 
 (function () {
-
+    if(s.length === 0) {
+        return 0;
+    }
+    const n = s.length;
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = s[0] !== '0' ? 1 : 0;
+    for(let i = 2; i <= n; i++) {
+        debugger
+        let first = Number(s.substring(i - 1, i));
+        let second = Number(s.substring(i - 2, i));
+        if(first >= 1 && first <= 9) {
+            dp[i] += dp[i-1];
+        }
+        if(second >= 10 && second <= 26) {
+            dp[i] += dp[i-2];
+        }
+    }
+    console.log(dp[n]);
+    return dp[n];
 })();
