aae74a581b2792d6ca79154118ecae795990d1f8
SkynI25 <rlxo1651@daum.net>
2020-06-10 11:18:08 +0900

    Leetcode94 solution

--- 50-100/leetcode94.js
+++ 50-100/leetcode94.js
@@ -18,5 +18,32 @@ function TreeNode(val, left, right) {
 let root = [1,null,2,3];
 
 (function() {
-
+    let treeList = [];
+    let stack = [];
+    let current = root;
+    while(current != null || stack.length !== 0) {
+        while(current !== null) {
+            stack.push(current);
+            current = current.left;
+        }
+        current = stack.pop();
+        treeList.push(current.val);
+        current = current.right;
+    }
+    console.log(treeList);
+    /* first solution
+    function helper(root, list) {
+        if(root !== null) {
+            if(root.left !== null) {
+                helper(root.left, list);
+            }
+            list.push(root.val);
+            if(root.right !== null) {
+                helper(root.right, list);
+            }
+        }
+    }
+    let treeList = [];
+    helper(root, treeList);
+    console.log(treeList); */
 })();
