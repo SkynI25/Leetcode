5781ff1783492c84845c6cdc77501f8994f6b737
SkynI25 <rlxo1651@daum.net>
2020-06-11 10:51:21 +0900

    Leetcode98 solution

--- 50-100/leetcode98.js
+++ 50-100/leetcode98.js
@@ -1,15 +1,23 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
+let root = [5,1,4,null,null,3,6];
 (function () {
-
+    function solve(root, min, max) {
+        if(root === null) {
+            return true;
+        }
+        if(root.val <= min || root.val >= max) {
+            return false;
+        }
+        return solve(root.left, min, root.val) && solve(root.right, root.val, max);
+    }
+    
+    return solve(root, -Infinity, Infinity);
 })();
