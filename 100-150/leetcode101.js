d71f63d5d0718b4d4702ca7a28d708f358fe0649
SkynI25 <rlxo1651@daum.net>
2020-06-11 17:15:19 +0900

    Leetcode101 solution

--- 100-150/leetcode101.js
+++ 100-150/leetcode101.js
@@ -1,5 +1,15 @@
-let root = "";
+let root = [1,2,2,3,4,4,3];
 
 (function () {
+    return solve(root, root);
     
+    function solve(t1, t2) {
+        if(t1 === null && t2 === null) {
+            return true;
+        }
+        if(t1 === null || t2 === null) {
+            return false;
+        }
+        return t1.val === t2.val && solve(t1.right, t2.left) && solve(t1.left, t2.right);
+    }
 })();
