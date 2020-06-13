0d363f087036a6165a8cd6329dcdbb04504ae750
SkynI25 <rlxo1651@daum.net>
2020-06-13 12:11:04 +0900

    Leetcode102 solution

--- 100-150/leetcode102.js
+++ 100-150/leetcode102.js
@@ -11,5 +11,26 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    
+    if(!root) {
+        return [];
+    }
+    const q = [];
+    q.push(root);
+    const result = [];
+    while(q.length > 0) {
+        const size = q.length;
+        const temp = [];
+        for(let i = 0; i < size; i++) {
+            const node = q.shift();
+            temp.push(node.val);
+            if(node.left) {
+                q.push(node.left);
+            }
+            if(node.right) {
+                q.push(node.right);
+            }
+        }
+        result.push(temp);
+    }
+    return result;
 };
