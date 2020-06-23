0bc3eafb7a485e672f52c7bba9ed951b57e2e810
SkynI25 <rlxo1651@daum.net>
2020-06-23 11:50:25 +0900

    Programmers problem

---
+++ programmers/0625/1P.js
@@ -0,0 +1,20 @@
+let A = [5,1,10,7];
+let B = [2,2,6,7];
+
+(function () {
+    let answer = 0;
+    let i = 0, j = 0;
+    let N = A.length;
+    A = A.sort((a,b) => b-a);
+    B = B.sort((a,b) => b-a);
+    while(i < N && j < N) {
+        if(B[j] <= A[i]) {
+            i++;
+        } else if(B[j] > A[i]) {
+            i++;
+            j++;
+            answer += 1;
+        }
+    }
+    console.log(answer);
+})();
