c6296daa96b45b6c5b819f1c635076390d1ca801
Ki Tae Park <rlxo1651@daum.net>
2020-08-01 23:27:10 +0900

    Programmers stack&queue problems

---
+++ programmers/stack&queue/1P.java
@@ -0,0 +1,7 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42584
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+        return answer;
+    }
+}
