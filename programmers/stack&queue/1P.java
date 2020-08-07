1db19f72cd912db24112b687b784b51849b53953
Ki Tae Park <rlxo1651@daum.net>
2020-08-08 00:54:54 +0900

    In stack&queue lists - stock problem solution

--- programmers/stack&queue/1P.java
+++ programmers/stack&queue/1P.java
@@ -1,7 +1,20 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42584
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = {};
+        int[] answer = new int[prices.length];
+        Arrays.fill(answer, 0);
+        for(int i = 0; i < prices.length; i++) {
+            int count = 0;
+            for(int j = i+1; j < prices.length; j++) {
+                count++;
+                if(prices[i] > prices[j]) {
+                    break;
+                }
+            }
+            answer[i] = count;
+        }
         return answer;
     }
 }
