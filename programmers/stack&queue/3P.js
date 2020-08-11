fe7e56581ced552b93b8a7b735c2b8805135f41a
Ki Tae Park <rlxo1651@daum.net>
2020-08-11 11:12:44 +0900

    stack&queue problems(feature develop, print)

--- programmers/stack&queue/3P.js
+++ programmers/stack&queue/3P.js
@@ -1,8 +1,31 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42586?language=javascript
-let bridge_length = 2;
-let weight = 10;
-let truck_weights = [7,4,5,6];
+let progresses = [93, 30, 55, 60, 40, 65];
+//[40, 93, 30, 55, 60, 65];
+let speeds = [1, 30, 5 , 10, 60, 7];
+//[60, 1, 30, 5 , 10, 7];
 
 (function () {
-
+    let answer = [];
+    debugger
+    while(progresses.length > 0 && speeds.length > 0) {
+        let workingDay = Math.ceil((100 - progresses[0]) / speeds[0]);
+        let lastIndex = -1;
+        let remainProgresses = speeds.map((speed, idx) => progresses[idx] += workingDay * speed);
+        let prevProgress = 0;
+        let completedFeature = remainProgresses.filter((progress, idx) => {
+            if((idx === 0 || prevProgress >= 100) && progress >= 100) {
+                lastIndex = idx;
+                prevProgress = progress;
+                return progress;
+            }
+            if(progress < 100) {
+                prevProgress = progress;
+            }
+        }).length;
+        completedFeature > 0 ? answer.push(completedFeature) : null;
+        progresses = lastIndex > -1 ? progresses.filter((progress, idx) => idx > lastIndex) : progresses;
+        speeds = lastIndex > -1 ? speeds.filter((speed, idx) => idx > lastIndex) : speeds;
+    }
+    console.log(answer);
+    return answer;
 })();
--- programmers/stack&queue/4P.js
+++ programmers/stack&queue/4P.js
@@ -1,7 +1,27 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42587
-let priorities = [2, 1, 3, 2];
+let priorities = [1, 2, 3, 4]; 
+//[1,1,9,1,1,1];
 let location = 2;
+//1;
 
 (function () {
+    debugger
+    let answer = 0;
+    let printIDs = new Array(priorities.length).fill(null).map((print,idx) => idx);
+        
+    while(priorities.length !== 0){
+        let first = priorities.shift();
 
+        if(priorities.some(current => current > first)) {
+            priorities.push(first);
+            printIDs.push(printIDs.shift());
+        } else if(printIDs[0] === location) {
+            answer += 1;
+            break;
+        } else {
+            printIDs.shift();
+            answer += 1;
+        }
+    }
+    return answer;
 })();
