becabbae0ffcd5ad7b3e04646ac8774d62014e48
Ki Tae Park <rlxo1651@daum.net>
2020-08-31 12:04:35 +0900

    H-index problem solution

--- programmers/heap&sort/SORT3.js
+++ programmers/heap&sort/SORT3.js
@@ -1,4 +1,19 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42747
+let citations = [3, 0, 6, 1, 5];
 (function () {
-
+    citations.sort((a,b) => b - a);
+    let answer = 0;
+    for(let i = 0; i < citations.length; i++) {
+        let citaNum = 0;
+        let currentCita = citations[i];
+        for(let j = 0; j < citations.length; j++) {
+            if(citations[j] >= currentCita) {
+                citaNum++;
+            }
+        }
+        if(currentCita >= citaNum) {
+            answer = Math.max(citaNum, answer);
+        }
+    }
+    console.log(answer);
 })();
