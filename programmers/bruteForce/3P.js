deb8909e496d9d8fb2ff73357dcfe56af86422d0
Ki Tae Park <rlxo1651@daum.net>
2020-09-23 23:03:34 +0900

    carpet problem solution

--- programmers/bruteForce/3P.js
+++ programmers/bruteForce/3P.js
@@ -1,4 +1,15 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42842
+let brown = 10;
+let yellow = 2;
 (function () {
-
+    const result = [];
+    const x = (brown-4)/2;
+    for (let i=1; i<=x; i++) {
+        if (i * (x-i) === red) {
+            result.push(x-i+2);
+            result.push(i+2);
+            break;
+        }
+    }
+    return result;
 })();
